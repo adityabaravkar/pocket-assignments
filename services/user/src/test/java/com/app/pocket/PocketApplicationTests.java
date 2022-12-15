445bfa7537969e0ff5023d024c373faf4e7cfe2e
Aditya Baravkar <aditya.baravkar@sjsu.edu>
2022-12-15 12:27:27 -0800

    Initialized user service project.

---
+++ services/user/src/test/java/com/app/pocket/PocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
